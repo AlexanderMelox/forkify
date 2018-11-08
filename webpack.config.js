f7b2042b26d642bd86d8aecccb4ebe5793eaeda2
Alexander Melo <alexandermelox@gmail.com>
2018-11-07 19:52:17 -0500

    setup the webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  // Entry point of app
   entry: './src/js/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: 'bundle.js'
-  }
+    // Relative path to output folder
+    path: path.resolve(__dirname, 'dist'),
+    // Name of the output file
+    filename: 'js/bundle.js'
+  },
+  devServer: {
+    // Folder in which to serve from
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
 };
