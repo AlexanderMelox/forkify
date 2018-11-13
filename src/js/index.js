b4bf99a075e4ada36f56d33e89ace917c7fdfb64
Alexander Melo <alexandermelox@gmail.com>
2018-11-13 18:47:38 -0500

    created the function for fetching the API data

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
-// Global app controller
-import num from './test';
+import axios from 'axios';
 
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+async function getResults(query) {
+  try {
+    const key = '3b9d3ef77f2b494ff44def3702431b75';
+    const proxy = 'https://api.codetabs.com/v1/proxy?quest=';
+    const results = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    const { recipes } = results.data;
+    console.log(recipes);
+  } catch(error) {
+    alert(error);
+  }
+} 
+getResults('pizza');
+
+// https://www.food2fork.com/api/search
