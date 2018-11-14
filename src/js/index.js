7e7bf022cb58af966ede687110fb60820c7e98b1
Alexander Melo <alexandermelox@gmail.com>
2018-11-13 19:05:52 -0500

    Created the Search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-  try {
-    const key = '3b9d3ef77f2b494ff44def3702431b75';
-    const proxy = 'https://api.codetabs.com/v1/proxy?quest=';
-    const results = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    const { recipes } = results.data;
-    console.log(recipes);
-  } catch(error) {
-    alert(error);
-  }
-} 
-getResults('pizza');
-
-// https://www.food2fork.com/api/search
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    const key = '3b9d3ef77f2b494ff44def3702431b75';
+    const proxy = 'https://api.codetabs.com/v1/proxy?quest=';
+    const apiURL = 'https://www.food2fork.com/api/search';
+    try {
+      const results = await axios(`${proxy}${apiURL}?key=${key}&q=${this.query}`);
+      this.results = results.data.recipes;
+    } catch(error) {
+      alert(error);
+    }
+  } 
+}
+
+export default Search;
